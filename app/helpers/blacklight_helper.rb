f1c0e3aef5a060d0625ec8b8bb835aca77729195
B Alter <barnaby.alter@gmail.com>
2013-04-08 18:31:53 -0400

    Param presence bugfix

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -4,6 +4,6 @@ module BlacklightHelper
   # Show base fields and extra fields if there are occurrences
   def should_show_index_field? document, field, solr_fname
     (["ead_id","title_txt","publisher_display","abstract_txt"].include? solr_fname or
-      (!params[:q].empty? and !excerpt((render_index_field_value :document => document, :field => solr_fname), params[:q]).nil?))
+      (params[:q].present? and !excerpt((render_index_field_value :document => document, :field => solr_fname), params[:q]).nil?))
   end
 end
