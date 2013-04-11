8e239c27c5f144583b88689257f32c2bb8f05590
B Alter <barnaby.alter@gmail.com>
2013-04-11 15:42:29 -0400

    Added reply-to :

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActionMailer::Base.default :from => 'no-reply@library.nyu.edu'
+
 # Initialize the rails application
 Findingaids::Application.initialize!
+
