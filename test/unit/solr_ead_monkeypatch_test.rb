1a686e7b53fac41c5e6897e82e57c284a03e4799
B Alter <barnaby.alter@gmail.com>
2013-04-16 17:20:15 -0400

    New VCRs

--- test/unit/solr_ead_monkeypatch_test.rb
+++ test/unit/solr_ead_monkeypatch_test.rb
@@ -12,13 +12,13 @@ class SolrEadMonkeyPatchTest < ActiveSupport::TestCase
     ENV['REPO'] = 'archives'
     indexer = SolrEad::Indexer.new(:document=>CustomDocument, :simple => true)
     assert_instance_of(SolrEad::Indexer, indexer)
-    VCR.use_cassette('update eads into solr', :match_requests_on => [:body]) do
+    VCR.use_cassette('update eads into solr', :match_requests_on => [:method]) do
       Dir.glob(File.join(Rails.root,"test","examples","*.xml")).each do |file|
         indexer.batch_update(file)
       end
       assert_instance_of(Array, indexer.solr_docs)
     end
-    VCR.use_cassette('commit into solr', :match_requests_on => [:body]) do
+    VCR.use_cassette('commit into solr', :match_requests_on => [:method]) do
       indexer.batch_commit
       assert_instance_of(RSolr::Client, indexer.solr)
     end
