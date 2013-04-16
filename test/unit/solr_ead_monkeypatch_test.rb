e7201a3353fdf710726fc34afe6ea16d03177894
B Alter <barnaby.alter@gmail.com>
2013-04-16 12:07:19 -0400

    Added dummy EAD to test

--- test/unit/solr_ead_monkeypatch_test.rb
+++ test/unit/solr_ead_monkeypatch_test.rb
@@ -13,7 +13,7 @@ class SolrEadMonkeyPatchTest < ActiveSupport::TestCase
       ENV['REPO'] = 'archives'
       indexer = SolrEad::Indexer.new(:document=>CustomDocument, :simple => true)
       assert_instance_of(SolrEad::Indexer, indexer)
-      indexer.update_without_commit(File.join(Rails.root, "data", "archives", "adler.xml"))
+      indexer.update_without_commit(File.join(Rails.root, "test", "examples", "adler.xml"))
       assert_instance_of(RSolr::Client, indexer.solr)
     end
   end
