fff75dc4456caa33cd8c81c71992bad6e4358bb6
B Alter <barnaby.alter@gmail.com>
2013-04-17 16:02:03 -0400

    Fixed syntax error

--- config/initializers/solr_ead_monkeypatch.rb
+++ config/initializers/solr_ead_monkeypatch.rb
@@ -15,34 +15,35 @@ module SolrEad
       end
       self.solr_docs = []
       # Retry on 503
-      self.solr = RSolr.connect(:url => url, :retry_503 => 1, :retry_after_limit => 1)
+      self.solr = RSolr.connect :url => url
+      #self.solr = RSolr.connect(:url => url, :retry_503 => 1, :retry_after_limit => 1)
       self.options = opts
     end
     
-  #  # Add multiple documents
-  #  def batch_update(file)
-  #    doc = om_document(File.new(file))
-  #    solr_doc = doc.to_solr
-  #    solr.delete_by_query( 'ead_id:"' + solr_doc["id"] + '"' )
-  #    solr_docs << solr_doc
-  #    add_components(file) unless options[:simple]
-  #  end
-  #  
-  #  def add_components(file)
-  #    counter = 1
-  #    components(file).each do |node|
-  #      solr_doc = om_component_from_node(node).to_solr(additional_component_fields(node))
-  #      solr_doc.merge!({"sort_i" => counter.to_s})
-  #      solr_docs << solr_doc
-  #      counter = counter + 1
-  #    end
-  #  end
-  #  
-  #  def batch_commit(batch = 500)
-  #    solr_docs.in_groups_of(batch.to_i).each do |solr_docs_arr|
-  #      solr.add solr_docs_arr.delete_if {|x| x.nil? }
-  #    end   
-  #    solr.commit
-  #  end
+    # Add multiple documents
+    def batch_update(file)
+      doc = om_document(File.new(file))
+      solr_doc = doc.to_solr
+      solr.delete_by_query( 'ead_id:"' + solr_doc["id"] + '"' )
+      solr_docs << solr_doc
+      add_components(file) unless options[:simple]
+    end
+    
+    #def add_components(file)
+    #  counter = 1
+    #  components(file).each do |node|
+    #    solr_doc = om_component_from_node(node).to_solr(additional_component_fields(node))
+    #    solr_doc.merge!({"sort_i" => counter.to_s})
+    #    solr_docs << solr_doc
+    #    counter = counter + 1
+    #  end
+    #end
+    
+    def batch_commit(batch = 500)
+      solr_docs.in_groups_of(batch.to_i).each do |solr_docs_arr|
+        solr.add solr_docs_arr.delete_if {|x| x.nil? }
+      end   
+      solr.commit
+    end
   end
 end
