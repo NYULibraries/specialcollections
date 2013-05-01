ce16923ec22144d61a94cc6307e5fc785b11ae1a
B Alter <barnaby.alter@gmail.com>
2013-05-01 15:33:58 -0400

    Reshuffle order of to_solr override function

--- lib/findingaids/custom_document.rb
+++ lib/findingaids/custom_document.rb
@@ -14,13 +14,13 @@ class CustomDocument < SolrEad::Document
     t.name(:index_as=>[:facetable])
     t.persname(:index_as=>[:facetable])
     t.subject(:index_as=>[:facetable])
-    
+
     # These terms are proxied to match with Blacklight's default facets, but otherwise
     # you can remove them or rename the above facet terms to match with your solr
     # implementation.
     t.subject_geo(:proxy=>[:geogname])
     t.subject_topic(:proxy=>[:subject])
-    
+
     t.title(:path=>"archdesc/did/unittitle", :index_as=>[:unstemmed, :displayable])
     t.title_filing(:path=>"titleproper", :attributes=>{ :type => "filing" }, :index_as=>[:sortable])
     t.title_num(:path=>"archdesc/did/unitid", :index_as=>[:searchable, :displayable])
@@ -34,60 +34,60 @@ class CustomDocument < SolrEad::Document
     t.langcode(:path=>"did/langmaterial/language/@langcode")
     t.abstract(:path=>"archdesc/did/abstract", :index_as=>[:searchable])
     t.sponsor(:path=>"sponsor", :index_as=>[:searchable,:displayable])
-    
+
     t.collection(:proxy=>[:title], :index_as=>[:facetable])
-    
-    #
-    ## General field available within archdesc
-    ##t.accessrestrict(:path=>"archdesc/accessrestrict/p", :index_as=>[:searchable])
-    ##t.accessrestrict_heading(:path=>"archdesc/accessrestrict/head", :index_as=>[:displayable])
-    ##.accruals(:path=>"archdesc/accruals/p", :index_as=>[:searchable])
-    ##.accruals_heading(:path=>"archdesc/accruals/head", :index_as=>[:displayable])
-    #t.acqinfo(:path=>"archdesc/acqinfo/p", :index_as=>[:searchable])
-    #t.acqinfo_heading(:path=>"archdesc/acqinfo/head", :index_as=>[:displayable])
-    ##t.altformavail(:path=>"archdesc/altformavail/p", :index_as=>[:searchable])
-    ##t.altformavail_heading(:path=>"archdesc/altformavail/head", :index_as=>[:displayable])
-    ##t.appraisal(:path=>"archdesc/appraisal/p", :index_as=>[:searchable])
-    ##t.appraisal_heading(:path=>"archdesc/appraisal/head", :index_as=>[:displayable])
-    ##t.arrangement(:path=>"archdesc/arrangement/p", :index_as=>[:searchable])
-    ##t.arrangement_heading(:path=>"archdesc/arrangement/head", :index_as=>[:displayable])
-    #t.bioghist(:path=>"archdesc/bioghist/p", :index_as=>[:searchable])
-    #t.bioghist_heading(:path=>"archdesc/bioghist/head", :index_as=>[:displayable])
-    ##t.bibliography(:path=>"archdesc/bibliography/bibref", :index_as=>[:searchable])
-    ##t.bibliography_heading(:path=>"archdesc/bibliography/head", :index_as=>[:displayable])
-    #t.custodhist(:path=>"archdesc/custodhist/p", :index_as=>[:searchable])
-    #t.custodhist_heading(:path=>"archdesc/custodhist/head", :index_as=>[:displayable])
-    ##t.fileplan(:path=>"archdesc/fileplan/p", :index_as=>[:searchable])
-    ##t.fileplan_heading(:path=>"archdesc/fileplan/head", :index_as=>[:displayable])
-    ##t.originalsloc(:path=>"archdesc/originalsloc/p", :index_as=>[:searchable])
-    ##t.originalsloc_heading(:path=>"archdesc/originalsloc/head", :index_as=>[:displayable])
-    #t.phystech(:path=>"archdesc/phystech/p", :index_as=>[:searchable])
-    #t.phystech_heading(:path=>"archdesc/phystech/head", :index_as=>[:displayable])
-    ##t.processinfo(:path=>"archdesc/processinfo/p", :index_as=>[:searchable])
-    ##t.processinfo_heading(:path=>"archdesc/processinfo/head", :index_as=>[:displayable])
-    #t.relatedmaterial(:path=>"archdesc/relatedmaterial/p", :index_as=>[:searchable])
-    #t.relatedmaterial_heading(:path=>"archdesc/relatedmaterial/head", :index_as=>[:displayable])
-    #t.separatedmaterial(:path=>"archdesc/separatedmaterial/p", :index_as=>[:searchable])
-    ##t.separatedmaterial_heading(:path=>"archdesc/separatedmaterial/head", :index_as=>[:displayable])
-    #t.controlaccess(:path => "archdesc/controlaccess", :index_as=>[:searchable])
-    #t.scopecontent(:path=>"archdesc/scopecontent/p", :index_as=>[:searchable])
-    #t.scopecontent_heading(:path=>"archdesc/scopecontent/head", :index_as=>[:displayable])
-    #t.userestrict(:path=>"archdesc/userestrict/p", :index_as=>[:searchable])
-    #t.userestrict_heading(:path=>"archdesc/userestrict/head", :index_as=>[:displayable])
-    #t.unittitle(:path=>"archdesc/dsc//c[@level='file']/did/unittitle", :index_as=>[:searchable])
-    #t.odd(:path=>"archdesc/dsc//odd", :index_as=>[:searchable])
-    #t.index(:path=>"archdesc/index", :index_as=>[:searchable])
-    #
-    #t.publisher(:path => "publisher", :index_as => [:searchable])
+
+
+    # General field available within archdesc
+    #t.accessrestrict(:path=>"archdesc/accessrestrict/p", :index_as=>[:searchable])
+    #t.accessrestrict_heading(:path=>"archdesc/accessrestrict/head", :index_as=>[:displayable])
+    #.accruals(:path=>"archdesc/accruals/p", :index_as=>[:searchable])
+    #.accruals_heading(:path=>"archdesc/accruals/head", :index_as=>[:displayable])
+    t.acqinfo(:path=>"archdesc/acqinfo/p", :index_as=>[:searchable])
+    t.acqinfo_heading(:path=>"archdesc/acqinfo/head", :index_as=>[:displayable])
+    #t.altformavail(:path=>"archdesc/altformavail/p", :index_as=>[:searchable])
+    #t.altformavail_heading(:path=>"archdesc/altformavail/head", :index_as=>[:displayable])
+    #t.appraisal(:path=>"archdesc/appraisal/p", :index_as=>[:searchable])
+    #t.appraisal_heading(:path=>"archdesc/appraisal/head", :index_as=>[:displayable])
+    #t.arrangement(:path=>"archdesc/arrangement/p", :index_as=>[:searchable])
+    #t.arrangement_heading(:path=>"archdesc/arrangement/head", :index_as=>[:displayable])
+    t.bioghist(:path=>"archdesc/bioghist/p", :index_as=>[:searchable])
+    t.bioghist_heading(:path=>"archdesc/bioghist/head", :index_as=>[:displayable])
+    #t.bibliography(:path=>"archdesc/bibliography/bibref", :index_as=>[:searchable])
+    #t.bibliography_heading(:path=>"archdesc/bibliography/head", :index_as=>[:displayable])
+    t.custodhist(:path=>"archdesc/custodhist/p", :index_as=>[:searchable])
+    t.custodhist_heading(:path=>"archdesc/custodhist/head", :index_as=>[:displayable])
+    #t.fileplan(:path=>"archdesc/fileplan/p", :index_as=>[:searchable])
+    #t.fileplan_heading(:path=>"archdesc/fileplan/head", :index_as=>[:displayable])
+    #t.originalsloc(:path=>"archdesc/originalsloc/p", :index_as=>[:searchable])
+    #t.originalsloc_heading(:path=>"archdesc/originalsloc/head", :index_as=>[:displayable])
+    t.phystech(:path=>"archdesc/phystech/p", :index_as=>[:searchable])
+    t.phystech_heading(:path=>"archdesc/phystech/head", :index_as=>[:displayable])
+    #t.processinfo(:path=>"archdesc/processinfo/p", :index_as=>[:searchable])
+    #t.processinfo_heading(:path=>"archdesc/processinfo/head", :index_as=>[:displayable])
+    t.relatedmaterial(:path=>"archdesc/relatedmaterial/p", :index_as=>[:searchable])
+    t.relatedmaterial_heading(:path=>"archdesc/relatedmaterial/head", :index_as=>[:displayable])
+    t.separatedmaterial(:path=>"archdesc/separatedmaterial/p", :index_as=>[:searchable])
+    #t.separatedmaterial_heading(:path=>"archdesc/separatedmaterial/head", :index_as=>[:displayable])
+    t.controlaccess(:path => "archdesc/controlaccess", :index_as=>[:searchable])
+    t.scopecontent(:path=>"archdesc/scopecontent/p", :index_as=>[:searchable])
+    t.scopecontent_heading(:path=>"archdesc/scopecontent/head", :index_as=>[:displayable])
+    t.userestrict(:path=>"archdesc/userestrict/p", :index_as=>[:searchable])
+    t.userestrict_heading(:path=>"archdesc/userestrict/head", :index_as=>[:displayable])
+    t.unittitle(:path=>"archdesc/dsc//c[@level='file']/did/unittitle", :index_as=>[:searchable])
+    t.odd(:path=>"archdesc/dsc//odd", :index_as=>[:searchable])
+    t.index(:path=>"archdesc/index", :index_as=>[:searchable])
+
+    t.publisher(:path => "publisher", :index_as => [:searchable])
  end
 
  # Optionally, you may tweak other solr fields here.  Otherwise, you can leave this
  # method out of your definition.
  def to_solr(solr_doc = Hash.new)
+   solr_doc.merge!({"publisher_unstem_search" =>      format_publisher(self.publisher)})
+   solr_doc.merge!({"repository_s" =>                 format_repository})
+   solr_doc.merge!({"heading_txt" =>                  ("Guide to the " + self.title.first + " (" + self.title_num.first + ")")}) unless self.title_num.empty?
    super(solr_doc)
-   #solr_doc.merge!({"publisher_unstem_search" => format_publisher(self.publisher)})
-   #solr_doc.merge!({"repository_s" => format_repository})
-   #solr_doc.merge!({"heading_txt" => ("Guide to the " + self.title.first + " (" + self.title_num.first + ")")}) unless self.title_num.empty?
  end
 
 end
