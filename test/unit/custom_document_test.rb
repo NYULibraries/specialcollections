81bef6de0a0b0eb562d2804f12b3a3734a18b434
B Alter <barnaby.alter@gmail.com>
2013-05-08 16:42:09 -0400

    Increase testing coverage for ead indexing

---
+++ test/unit/custom_document_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class CustomDocumentTest < ActiveSupport::TestCase
+  
+  test "solr ead index file" do
+    VCR.use_cassette('solr_ead index test record', :match_requests_on => [:body]) do
+      ENV['FILE'] = "#{Rails.root}/test/dummy/data/archives/adler.xml"
+      indexer = SolrEad::Indexer.new(:document => CustomDocument, :component => CustomComponent)
+      assert_nothing_raised do
+        indexer.update(ENV['FILE'])
+      end
+      assert_instance_of(SolrEad::Indexer, indexer)
+    end
+  end
+  
+  test "solr ead index directory" do
+    VCR.use_cassette('solr_ead index test directory', :match_requests_on => [:body]) do
+      ENV['DIR'] = "#{Rails.root}/test/dummy/data/archives"
+      indexer = SolrEad::Indexer.new(:document => CustomDocument, :component => CustomComponent)
+      Dir.glob(File.join(ENV['DIR'],"*")).each do |file|
+        assert_nothing_raised do
+          indexer.update(file)
+        end
+      end
+      assert_instance_of(SolrEad::Indexer, indexer)        
+    end
+  end
+  
+end
