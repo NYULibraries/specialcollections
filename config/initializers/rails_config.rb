d695447ee7a7dfbe4465aae791e9573d571a0b37
B Alter <barnaby.alter@gmail.com>
2013-07-01 18:41:10 -0400

    Add newrelic.yml to load order

--- config/initializers/rails_config.rb
+++ config/initializers/rails_config.rb
@@ -3,7 +3,7 @@ RailsConfig.setup do |config|
 end
 ActiveSupport.on_load(:before_initialize) do
   # Add other global settings files
-  other_files = ["institutions", "sfx_databases", "solr", "sunspot", "capistrano", "authpds", "#{Rails.env}"]
+  other_files = ["institutions", "sfx_databases", "solr", "sunspot", "capistrano", "authpds", "newrelic", "#{Rails.env}"]
   RailsConfig.load_and_set_settings(
     Rails.root.join("config", "settings.yml").to_s,
     *other_files.collect { |setting| Rails.root.join("config", "settings", "#{setting}.yml").to_s },
