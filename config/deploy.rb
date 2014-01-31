8f64ceff9e75293dce378433b5e366b09c04f68b
B Alter <barnaby.alter@gmail.com>
2014-01-31 12:59:57 -0500

    Post build task removes eads from current deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,2 +1,10 @@
 require 'nyulibraries/deploy/capistrano'
 set :app_title, "findingaids"
+
+namespace :deploy do
+  task :remove_eads do
+    run "rm -rf #{current_path}/data"
+  end
+end
+
+after "deploy", "deploy:remove_eads"
