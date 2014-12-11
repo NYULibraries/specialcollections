0c4e0ed36e9362df39a24932efcfa4912e976d7d
B Alter <barnaby.alter@gmail.com>
2014-12-11 17:10:14 -0500

    Updating Boostrap3 styles too look good

--- app/views/layouts/application.rb
+++ app/views/layouts/application.rb
@@ -2,11 +2,11 @@
 module Views
   module Layouts
     class Application < ActionView::Mustache
-      
+
       def application_title
         t("application_title")
       end
-      
+
       # Print breadcrumb navigation
       def breadcrumbs
         breadcrumbs = super
@@ -15,52 +15,41 @@ module Views
         breadcrumbs << params[:repository] if params[:repository].present?
         return breadcrumbs
       end
-      
+
       # Render footer partial
       def footer_html
         render :partial => 'shared/footer'
       end
-      
+
       # Prepend modal dialog elements to the body
       def prepend_body
-        prepend_body = '<div class="modal-container"></div>'.html_safe
-        prepend_body << '<div id="ajax-modal" class="modal hide fade" tabindex="-1"></div>'.html_safe
+        render partial: 'shared/ajax_modal'
       end
-      
+
       # Prepend search box and flash message partials before to yield
       def prepend_yield
         return unless show_search_box?
+
         prepend_yield = ""
-        
-        prepend_yield += render :partial => 'shared/header_navbar'
-      
-        prepend_yield += content_tag :div, :id => "main-flashses" do
-         render :partial => '/flash_msg'
-        end
-        
+        prepend_yield = render :partial => 'shared/header_navbar'
         return prepend_yield.html_safe
       end
-      
+
       # Boolean for whether or not to show tabs
       def show_tabs
-        return false
+        false
       end
-      
+
       # Only show search box on admin view or for search catalog, excluding bookmarks, search history, etc.
       def show_search_box?
-        return (!controller.controller_name.eql? "bookmarks")
+        !controller.controller_name.eql? "bookmarks"
       end
-  
-      # Print default blacklight onload code
-      def onload
-        "$('input#q').focus();" if params[:q].to_s.empty? and params[:f].to_s.empty? and params[:id].nil?
-      end
-      
+
       # Add blacklight body classes to layout
       def body_class
-        render_body_class.html_safe    
+        render_body_class.html_safe
       end
-      
+
     end
   end
 end
