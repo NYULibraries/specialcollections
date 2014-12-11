fbd89d2a1726bbd3d2e863c8ad1306c48f9cc9cd
B Alter <barnaby.alter@gmail.com>
2014-12-11 16:03:53 -0500

    Cleaned up assets structure and for use with Rails4

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w[
+  nyulibraries/nyu/header.png
+  magnifier.png
+]
