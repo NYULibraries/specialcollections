9d03d4cb26c605b9e3be9c88eef80cea3ebdb7eb
B Alter <barnaby.alter@gmail.com>
2014-12-12 12:08:58 -0500

    Force ruby version in deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,8 @@ require 'formaggio/capistrano'
 
 set :app_title, "findingaids"
 
+set :rvm_ruby_string, "2.1.5"
+
 namespace :deploy do
   task :remove_eads do
     run "rm -rf #{current_path}/data"
