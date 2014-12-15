95a8e8180ca594b6f537f8f36df9317719cc0c90
B Alter <barnaby.alter@gmail.com>
2014-12-15 15:38:39 -0500

    Include subdirectories in autoload as well

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,6 @@ module Findingaids
     # config.i18n.default_locale = :de
 
     # Autoload the lib path
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
   end
 end
