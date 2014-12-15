e9b6de120339925d9dfdeb86ce6b0d9ea31d1100
B Alter <barnaby.alter@gmail.com>
2014-12-15 18:43:08 -0500

    Removed outdated vcr test from catalog spec

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
 describe CatalogController do
-  
+
   def assigns_response
     @controller.instance_variable_get("@response")
   end
-  
-  describe ".index" do
-    it "should return some results", vcr: { cassette_name: "basic search results" } do
-      get :index, :q => "bytsura", :search_field => "all_fields"
-      
-      assigns_response.docs.size.should > 1
-      assigns_response.facets.size.should > 1
+
+  describe "#index" do
+    it "should return some results" do
+      get :index, :q => "bloch", :search_field => "all_fields"
+
+      assigns_response.docs.size.should > 0
+      assigns_response.facets.size.should > 0
     end
   end
-  
+
 end
