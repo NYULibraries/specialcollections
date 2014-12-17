b8bc30bbacc4f1f31a524c6ed6a44e792bcda59b
B Alter <barnaby.alter@gmail.com>
2014-12-17 17:04:53 -0500

    Make repositories hash with indifferent access

--- lib/findingaids/repositories.rb
+++ lib/findingaids/repositories.rb
@@ -3,7 +3,7 @@ module Findingaids
     include Enumerable
 
     def self.repositories
-      @repositories ||= YAML.load_file( File.join(Rails.root, "config", "repositories.yml") )["Catalog"]["repositories"]
+      @repositories ||= YAML.load_file( File.join(Rails.root, "config", "repositories.yml") )["Catalog"]["repositories"].with_indifferent_access
     end
 
     extend Forwardable
