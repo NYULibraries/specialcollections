2e2f4f4482891abbb6d41fa86a606e56df2c2149
B Alter <barnaby.alter@gmail.com>
2014-12-17 16:59:10 -0500

    Added factories for reusable classes

---
+++ spec/factories/solr_documents.rb
@@ -0,0 +1,40 @@
+FactoryGirl.define do
+  factory :solr_document do
+    skip_create
+    initialize_with do
+      new(
+      {
+        id: id,
+        format_ssm: format,
+        title_ssm: title,
+        abstract_ssm: abstract,
+        repository_ssi: repository,
+        ead_ssi: ead,
+        bioghist_ssm: bioghist,
+        custodhist_ssm: custodhist,
+        ref_ssi: ref,
+        parent_ssm: parent,
+        heading_ssm: heading,
+        parent_unittitles_ssm: parent_unittitles,
+        collection_ssm: collection,
+        score: score
+        }, solr_response
+        )
+      end
+      solr_response { create(:solr_response) }
+      id { 'testead123' }
+      format { ["Archival Collection"] }
+      title { ["The Title"] }
+      abstract { "Interesting abstract" }
+      repository { "fales" }
+      ead { "testead" }
+      bioghist { ['title'] }
+      custodhist { ['publisher'] }
+      ref { "123" }
+      parent { nil }
+      heading { ["Guide to titling finding aids"] }
+      parent_unittitles { nil }
+      collection { ["Bytsura Collection of Things"] }
+      score { 4.944287 }
+    end
+  end
