6ef839010d6f9e5c51dab99ccb9c1552a60f2a3d
B Alter <barnaby.alter@gmail.com>
2015-02-12 17:20:58 -0500

    Narrowing down where we pull the collection information from on index

--- lib/findingaids/ead/document.rb
+++ lib/findingaids/ead/document.rb
@@ -2,58 +2,58 @@ class Findingaids::Ead::Document < SolrEad::Document
 
   include Findingaids::Ead::Behaviors
 
-  use_terminology SolrEad::Document
+  # Define each term in your ead that you want put into the solr document
+  set_terminology do |t|
+    t.root(path:"ead")
+    t.eadid
+    t.unittitle(path:"archdesc[@level='collection']/did/unittitle",index_as:[:searchable,:displayable])
+    t.unitid(path:"archdesc[@level='collection']/did/unitid",index_as:[:searchable,:displayable])
+    t.langcode(path:"archdesc[@level='collection']/did/langmaterial/language/@langcode")
+    t.unitdate(path:"archdesc[@level='collection']/did/unitdate/@normal",index_as:[:displayable,:searchable,:facetable])
+    t.abstract(path:"archdesc[@level='collection']/did/abstract",index_as:[:searchable,:displayable])
+    t.creator(path:"archdesc[@level='collection']/did/origination[@label='creator']",index_as:[:searchable,:displayable])
+    t.scopecontent(path:"archdesc[@level='collection']/scopecontent/p",index_as:[:searchable])
+    t.bioghist(path:"archdesc[@level='collection']/bioghist/p",index_as:[:searchable])
+    t.acqinfo(path:"archdesc[@level='collection']/acqinfo/p",index_as:[:searchable])
+    t.custodhist(path:"archdesc[@level='collection']/custodhist/p",index_as:[:searchable])
+    t.appraisal(path:"archdesc[@level='collection']/appraisal/p",index_as:[:searchable])
+    t.chronlist(path:"archdesc[@level='collection']/bioghist/chronlist/chronitem/event",index_as:[:searchable])
+    t.corpname(path:"archdesc[@level='collection']/corpname",index_as:[:searchable,:displayable])
+    t.famname(path:"archdesc[@level='collection']/famname",index_as:[:searchable,:displayable])
+    t.function(path:"archdesc[@level='collection']/function",index_as:[:searchable,:displayable])
+    t.genreform(path:"archdesc[@level='collection']/genreform",index_as:[:searchable,:displayable])
+    t.geogname(path:"archdesc[@level='collection']/geogname",index_as:[:searchable,:displayable])
+    t.name(path:"archdesc[@level='collection']/name",index_as:[:searchable,:displayable])
+    t.occupation(path:"archdesc[@level='collection']/occupation",index_as:[:searchable,:displayable])
+    t.persname(path:"archdesc[@level='collection']/persname",index_as:[:searchable,:displayable])
+    t.subject(path:"archdesc[@level='collection']/subject",index_as:[:searchable,:displayable])
+    t.title(path:"archdesc[@level='collection']/title",index_as:[:searchable,:displayable])
+    t.note(path:"archdesc[@level='collection']/note",index_as:[:searchable,:displayable])
 
-  # Extend terminology from presets
-  # https://github.com/awead/solr_ead/blob/master/lib/solr_ead/document.rb
-  extend_terminology do |t|
-    t.title(:path=>"archdesc[@level='collection']/did/unittitle", :index_as=>[:unstemmed_searchable])
-    t.title_num(:path=>"archdesc/did/unitid", :index_as=>[:searchable, :displayable])
-    t.abstract(:path=>"archdesc/did/abstract", :index_as=>[:searchable, :displayable])
-    t.sponsor(:path=>"sponsor", :index_as=>[:searchable,:displayable])
-
-    # General field available within archdesc
-    t.acqinfo(:path=>"archdesc/acqinfo/p", :index_as=>[:searchable, :displayable])
-    t.bioghist(:path=>"archdesc/bioghist/p", :index_as=>[:searchable, :displayable])
-    t.custodhist(:path=>"archdesc/custodhist/p", :index_as=>[:searchable, :displayable])
-    t.phystech(:path=>"archdesc/phystech/p", :index_as=>[:searchable, :displayable])
-    t.relatedmaterial(:path=>"archdesc/relatedmaterial/p", :index_as=>[:searchable, :displayable])
-    t.controlaccess(:path => "archdesc/controlaccess", :index_as=>[:searchable, :displayable])
-    t.scopecontent(:path=>"archdesc/scopecontent/p", :index_as=>[:searchable, :displayable])
-    t.index(:path=>"archdesc/index", :index_as=>[:searchable, :displayable])
-
-    # These are component level items indexed into the document as fulltext searchable fields
-    # so Blacklight can search it at the top level. Once this simple search is done we can search
-    # the components which are individually indexed by solr_ead to get additional info
-    #t.unittitle(:path=>"archdesc/dsc//c[@level='file' or @level='item']/did/unittitle", :index_as=>[:searchable, :displayable])
-    #t.odd(:path=>"archdesc/dsc//c[@level='file' or @level='item']/odd", :index_as=>[:searchable, :displayable])
-
-    t.title_filing(:path=>"archdesc[@level='collection']/did/unittitle", :index_as=>[:sortable])
-    t.date_filing(:path=>"archdesc/unitdate[@type='inclusive']/@normal", :index_as=>[:sortable])
-
-    t.publisher(:path => "publisher", :index_as => [:searchable])
-    t.dsc
-    t.material_type(proxy: [:genreform], :index_as=>[:facetable, :displayable])
-    t.collection(:proxy=>[:title], :index_as=>[:facetable, :displayable])
+    #Facets
+    t.collection(proxy:[:unittitle],index_as:[:facetable,:displayable])
+    t.material_type(proxy:[:genreform],index_as:[:facetable,:displayable])
   end
 
   def to_solr(solr_doc = Hash.new)
     solr_doc = super(solr_doc)
 
+    # Populate repository code from indexing folder structure
     Solrizer.insert_field(solr_doc, "repository",   format_repository,      :stored_sortable, :facetable)
-    Solrizer.insert_field(solr_doc, "heading",      heading_display,        :displayable) unless self.title_num.empty?
+    # Create a formatted heading based on the title and unit id
+    Solrizer.insert_field(solr_doc, "heading",      heading_display,        :displayable) unless self.unitid.empty?
+    # Set the format to Archival Collection
     Solrizer.insert_field(solr_doc, "format",       "Archival Collection",  :facetable, :displayable)
+    # Sortable element based on the type of thing this is, will help us sort by Collection, Series, etc.
     Solrizer.insert_field(solr_doc, "format",       0,                      :sortable)
-    Solrizer.insert_field(solr_doc, "unitdate",     ead_date_display,       :displayable)
+    # Special formatting for some fields and facets
     Solrizer.insert_field(solr_doc, "creator",      get_ead_creators,       :displayable, :facetable)
     Solrizer.insert_field(solr_doc, "name",         get_ead_names,          :facetable)
-    Solrizer.insert_field(solr_doc, "place",        get_ead_places,         :displayable, :facetable)
-
-    Solrizer.set_field(solr_doc, "subject",         get_ead_subject_facets, :facetable)
+    Solrizer.insert_field(solr_doc, "subject",      get_ead_subject_facets, :facetable)
     Solrizer.insert_field(solr_doc, "dao",          get_ead_dao_facet,      :facetable)
 
     # Replace certain fields with their html-formatted equivlents
-    Solrizer.set_field(solr_doc, "title", self.term_to_html("title"), :displayable)
+    Solrizer.set_field(solr_doc, "unittitle", self.term_to_html("unittitle"), :displayable)
 
     # Set lanuage codes
     solr_doc.merge!(ead_language_fields)
@@ -62,8 +62,12 @@ class Findingaids::Ead::Document < SolrEad::Document
   end
 
   protected
+  ##
+  # Create a heading display for archival collections
+  #
+  # self(title: "Interesting Items", title_num: "ABC.123") => "Guide to the Interesting Items (ABC.123)"
   def heading_display
-    "Guide to the " + self.term_to_html("title") + " (" + self.title_num.first + ")"
+    "Guide to the " + self.term_to_html("unittitle") + " (" + self.unitid.first + ")"
   end
 
 end
