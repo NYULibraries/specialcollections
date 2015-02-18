402a546cdccf2251017b9fab505e679a55bb8f86
B Alter <barnaby.alter@gmail.com>
2015-02-18 14:31:59 -0500

    Index the rest of the relevant fields for collection and round out rspec testing

--- lib/findingaids/ead/document.rb
+++ lib/findingaids/ead/document.rb
@@ -25,17 +25,17 @@ class Findingaids::Ead::Document < SolrEad::Document
     # Find the following wherever they exist in the tree structure under <archdesc level="collection">
     # except under the inventory which starts at <dsc>
     t.chronlist(path:"archdesc[@level='collection']/*[name() != 'dsc']//chronlist/chronitem/*/text()",index_as:[:searchable])
-    t.corpname(path:"archdesc[@level='collection']/corpname",index_as:[:searchable,:displayable])
-    t.famname(path:"archdesc[@level='collection']/famname",index_as:[:searchable,:displayable])
-    t.function(path:"archdesc[@level='collection']/function",index_as:[:searchable,:displayable])
-    t.genreform(path:"archdesc[@level='collection']/genreform",index_as:[:searchable,:displayable])
-    t.geogname(path:"archdesc[@level='collection']/geogname",index_as:[:searchable,:displayable])
-    t.name(path:"archdesc[@level='collection']/name",index_as:[:searchable,:displayable])
-    t.occupation(path:"archdesc[@level='collection']/occupation",index_as:[:searchable,:displayable])
-    t.persname(path:"archdesc[@level='collection']/persname",index_as:[:searchable,:displayable])
-    t.subject(path:"archdesc[@level='collection']/subject",index_as:[:searchable,:displayable])
-    t.title(path:"archdesc[@level='collection']/title",index_as:[:searchable,:displayable])
-    t.note(path:"archdesc[@level='collection']/note",index_as:[:searchable,:displayable])
+    t.corpname(path:"archdesc[@level='collection']/*[name() != 'dsc']//corpname",index_as:[:searchable,:displayable])
+    t.famname(path:"archdesc[@level='collection']/*[name() != 'dsc']//famname",index_as:[:searchable,:displayable])
+    t.function(path:"archdesc[@level='collection']/*[name() != 'dsc']//function",index_as:[:searchable,:displayable])
+    t.genreform(path:"archdesc[@level='collection']/*[name() != 'dsc']//genreform",index_as:[:searchable,:displayable])
+    t.geogname(path:"archdesc[@level='collection']/*[name() != 'dsc']//geogname",index_as:[:searchable,:displayable])
+    t.name(path:"archdesc[@level='collection']/*[name() != 'dsc']//name",index_as:[:searchable,:displayable])
+    t.occupation(path:"archdesc[@level='collection']/*[name() != 'dsc']//occupation",index_as:[:searchable,:displayable])
+    t.persname(path:"archdesc[@level='collection']/*[name() != 'dsc']//persname",index_as:[:searchable,:displayable])
+    t.subject(path:"archdesc[@level='collection']/*[name() != 'dsc']//subject",index_as:[:searchable,:displayable])
+    t.title(path:"archdesc[@level='collection']/*[name() != 'dsc']//title",index_as:[:searchable,:displayable])
+    t.note(path:"archdesc[@level='collection']/*[name() != 'dsc']//note",index_as:[:searchable,:displayable])
 
     #Facets
     t.collection(proxy:[:unittitle],index_as:[:facetable,:displayable])
--- spec/lib/findingaids/ead/document_spec.rb
+++ spec/lib/findingaids/ead/document_spec.rb
@@ -9,32 +9,48 @@ describe Findingaids::Ead::Document do
   # Check "include" because these are all multi-valued fields in Solr
   its(:eadid) { should include "Resource-EAD-ID-AT" }
   its(:unittitle) { should include "Resource--Title-AT" }
+  # Proxy for unittitle at the collection level
+  its(:collection) { should include "Resource--Title-AT" }
   its(:unitid) { should include "Resource.ID.AT.AT" }
   its(:langcode) { should include "eng" }
   its(:unitdate) { should include "1960/1970" }
   its(:abstract) { should include "Resource-Abstract-AT" }
   its(:creator) { should include "CNames-PrimaryName-AT. CNames-Subordinate1-AT. CNames-Subordiate2-AT. (CNames-Number-AT) (CNames-Qualifier-AT)" }
-  # its(:scopecontent) { should_not be_empty }
-  # its(:bioghist) { should_not be_empty }
-  # its(:acqinfo) { should_not be_empty }
-  # its(:custodhist) { should include "" }
-  # its(:appraisal) { should_not be_empty }
+  its(:scopecontent) { should include "Resource-ScopeContents-AT" }
+  its(:scopecontent) { should_not include "Resource-C01-ScopecontentNoteContent-AT" }
+  its(:bioghist) { should include "Resource-BiographicalHistorical-AT" }
+  its(:bioghist) { should_not include "c06--biogHist-part1" }
+  its(:acqinfo) { should include "Resource-ImmediateSourceAcquisition" }
+  its(:custodhist) { should include "Resource--CustodialHistory-AT" }
+  its(:appraisal) { should include "Resource-Appraisal-AT" }
   its(:chronlist) { should include "Christmas 1985" }
   its(:chronlist) { should_not include "first date" }
-  # its(:corpname) { should include "" }
-  # its(:famname) { should include "" }
-  # its(:function) { should include "" }
-  # its(:genreform) { should include "" }
-  # its(:geogname) { should include "" }
-  # its(:name) { should include "" }
-  # its(:occupation) { should include "" }
-  # its(:persname) { should include "" }
-  # its(:subject) { should include "" }
-  # its(:title) { should include "" }
-  # its(:note) { should include "" }
+  its(:corpname) { should include "CNames-PrimaryName-AT. CNames-Subordinate1-AT. CNames-Subordiate2-AT. (CNames-Number-AT) (CNames-Qualifier-AT)" }
+  its(:corpname) { should_not include "CNames-PrimaryName-AT. CNames-Subordinate1-AT. CNames-Subordiate2-AT. (CNames-Number-AT) (CNames-Qualifier-AT) -- Pictorial works" }
+  its(:famname) { should include "FNames-FamilyName-AT, FNames-Prefix-AT, FNames-Qualifier-AT -- Archives" }
+  its(:famname) { should_not include "FNames-FamilyName-AT, FNames-Prefix-AT, FNames-Qualifier-AT -- Pictorial works" }
+  its(:function) { should include "Subjects--Function-AT" }
+  its(:function) { should_not include "c06-index1" }
+  its(:genreform) { should include "Subjects--GenreForm--AT" }
+  # Proxy for genreform
+  its(:material_type) { should include "Subjects--GenreForm--AT" }
+  its(:genreform) { should_not include "Bike 1" }
+  its(:geogname) { should include "Subjects--Geographic Name--AT" }
+  # No lower level examples, so make sure the value doesn't repeat
+  its("geogname.count") { should eql 1 }
+  its(:name) { should be_empty } # No examples for name
+  its(:occupation) { should include "Subjects--Occupation--AT" }
+  its("occupation.count") { should eql 1 }
+  its(:persname) { should include "PNames-Primary-AT, PNames-RestOfName-AT, PNames-Prefix-AT, PName-Number-AT, PNames-Suffix-AT, PNames-Title-AT,  (PNames-FullerForm-AT), PNames-Dates-AT, PNames-Qualifier-AT" }
+  # Only 3 persnames at the top-level, don't include the lower ones
+  its("persname.count") { should eql 3 }
+  its(:subject) { should include "Subjects--Topical Term--AT" }
+  its("subject.count") { should eql 1 }
+  its(:title) { should include "Subjects--Uniform Title--AT" }
+  its("title.count") { should eql 1 }
+  its(:note) { should be_empty } # No examples for note
 
   describe "#to_solr" do
-
   end
 
   describe "facets" do
