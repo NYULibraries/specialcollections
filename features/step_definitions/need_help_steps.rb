7a34e5a9b38b68ac2b83ef0743993a471962936f
jgpawletko <jgpawletko@gmail.com>
2015-03-18 15:44:43 -0400

    add step_definitions/need_help_steps.rb

---
+++ features/step_definitions/need_help_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I see a link "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I clink on the "(.*?)" link$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a pop\-up window labeled "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the pop\-up window should contain the text "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
