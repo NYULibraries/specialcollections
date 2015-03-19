0a1f96d29929389ce19b18cc4d2d97cf7a9e9193
eshadatta <ed64@nyu.edu>
2015-03-19 15:14:24 -0400

    refactoring code to use series facet link

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -73,8 +73,8 @@ class CatalogController < ApplicationController
     #   The ordering of the field names is the order of the display
     #config.add_index_field solr_name("unittitle",         :displayable),  :label => "Title", :highlight => true, :helper_method => :render_field_item
     config.add_index_field solr_name("format",            :displayable),  :label => "Format", :helper_method => :render_field_item
-    #config.add_index_field solr_name("breadcrumb",        :displayable),  :label => "Contained in", :helper_method => :render_breadcrumb
-    config.add_index_field solr_name("parent_unittitles", :displayable),  :label => "Series", :highlight => true, :helper_method => :render_series_facet_link
+    config.add_index_field solr_name("breadcrumb",        :displayable),  :label => "Contained in", :helper_method => :render_breadcrumb
+    config.add_index_field solr_name("parent_unittitles", :displayable),  :label => "Contained in", :highlight => true, :helper_method => :render_series_facet_link
     config.add_index_field solr_name("unitdate",          :displayable),  :label => "Date range", :helper_method => :render_field_item
     config.add_index_field solr_name("abstract",          :displayable),  :label => "Abstract", :highlight => true, :helper_method => :render_field_item
     #config.add_index_field solr_name("language",          :displayable),  :label => "Language", :helper_method => :render_field_item
