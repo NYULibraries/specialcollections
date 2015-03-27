bef3f3e63e57eeb88af96db82b6172e10d30d75d
jgpawletko <jgpawletko@gmail.com>
2015-03-27 16:00:19 -0400

    remove unused cases from help_helper.rb

--- features/support/helpers/help_helper.rb
+++ features/support/helpers/help_helper.rb
@@ -3,8 +3,6 @@ module FindingaidsFeatures
     def text_to_enclosing_id(text)
       case text
       when 'Need Help?'  then 'sidebar'
-      when 'Search tips' then 'need_help_links'
-      when 'Special Collections contact information and hours' then 'need_help_links'
       else fail "unrecognized text: #{text}"
       end
     end
