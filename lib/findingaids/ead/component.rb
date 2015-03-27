2d234e416a66b1d227665c226f7d2339bc1ca016
eshadatta <ed64@nyu.edu>
2015-03-27 12:13:00 -0400

    removed breadcrumb field. Used existing functionality to only change display to show breadcrumb

--- lib/findingaids/ead/component.rb
+++ lib/findingaids/ead/component.rb
@@ -65,7 +65,6 @@ class Findingaids::Ead::Component < SolrEad::Component
     Solrizer.insert_field(solr_doc, "dao",        get_ead_dao_facet,                            :facetable)
     Solrizer.insert_field(solr_doc, "place",      get_ead_places,                               :displayable, :facetable)
     Solrizer.insert_field(solr_doc, "subject",    get_ead_subject_facets,                       :facetable)
-    Solrizer.insert_field(solr_doc, "breadcrumb", breadcrumb(solr_doc),                        :displayable)
 
     # Get the collection field
     Solrizer.set_field(solr_doc,    "collection", collection_name(solr_doc),                    :searchable, :displayable, :facetable)
