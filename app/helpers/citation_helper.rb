1cca49ef553c91a26e3e0ba5ae57e1540166d5f4
eshadatta <ed64@nyu.edu>
2015-03-31 14:00:09 -0400

    formatting citations per user story

--- app/helpers/citation_helper.rb
+++ app/helpers/citation_helper.rb
@@ -4,14 +4,24 @@ module CitationHelper
   # Render citation field value
   # hash contains order in which fields should be output for citation
   def render_citation_field(doc)
-    display_fields_hsh = {1 => "unittitle_ssm",2 => "unitdate_ssm",3=>"collection_ssm",4=>"unitid_ssm",5=>"location_ssm",6=>"repository_ssi"}
+    display_fields_hsh = { 1 =>  Solrizer.solr_name("unittitle",  :displayable),
+                           2 =>  Solrizer.solr_name("unitdate",   :displayable),
+                           3 =>  Solrizer.solr_name("collection", :displayable),
+                           4 =>  Solrizer.solr_name("unitid", :displayable),
+                           5 =>  Solrizer.solr_name("location", :displayable),
+                           6 =>  Solrizer.solr_name("repository", :stored_sortable),
+                           7 =>  Solrizer.solr_name("address", :searchable)
+                        }
     display_fields_hsh.keys.sort
     cite = []
+    cite_title = ""
     display_fields_hsh.each_pair{ |order,solr_name|
       cite << render_index_field_value(:document => doc, :field => solr_name) 
     }
+    title = cite.slice!(0) if Solrizer.solr_name("unittitle",  :displayable)
+    title = "#{title}, "
+    [title,cite.reject(&:blank?).join("; ")].join("").html_safe
 
-    cite.reject(&:blank?).join("; ").html_safe
   end
 end
 
