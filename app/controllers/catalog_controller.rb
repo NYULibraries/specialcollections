f334cbdb899b0042a178a2717d4f99ebf432914f
B Alter <barnaby.alter@gmail.com>
2015-04-07 17:20:08 -0400

    Removed sorting feature

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -94,8 +94,7 @@ class CatalogController < ApplicationController
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
-    config.add_sort_field "score desc, title_filing_si asc, format_si desc",                   :label => "relevance"
-    config.add_sort_field "date_filing_si desc, title_filing_si asc, format_si desc",          :label => "date"
+    config.add_sort_field "score desc",                   :label => "relevance"
 
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
