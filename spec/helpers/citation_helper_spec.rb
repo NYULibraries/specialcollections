cc29c78f7e92d522d9820b92b3a25b79f0332348
eshadatta <ed64@nyu.edu>
2015-04-13 14:29:34 -0400

    initial creation of solr doc for citations to include all fields necessary for acceptance criteria

--- spec/helpers/citation_helper_spec.rb
+++ spec/helpers/citation_helper_spec.rb
@@ -8,10 +8,12 @@ describe CitationHelper do
 
   let(:repositories) {{"fales" => {"display" => "The Fales Library"}}}
   let(:heading) { "Guide to titling finding aids" }
+  let(:location) { "Box:2, Folder: 3" }
+  let(:unitdate) { "Guide to titling finding aids" }
   let(:params) {{:controller => "catalog", :action => "index"}}
   let(:default_sort_hash) { {} }
   let(:field) { "unittitle_ssm" }
-  let(:solr_document) { create(:solr_document) }
+  let(:solr_document) { create(:solr_document_citation) }
   let(:document) {{ document: solr_document, field: field }}
   let(:blacklight_config) { create(:blacklight_config) }
   let(:source_params) do
@@ -27,10 +29,10 @@ describe CitationHelper do
   end
 
   describe "#render_citation_field" do
-    let(:solr_document) { create(:solr_document, unittitle: ["The Title"]) }
+    let(:solr_document) { create(:solr_document_citation, unittitle: ["The Title"]) }
     subject { render_citation_field(document[:document]) }
     context "citation is present" do
-      it { should eql("The Title, Bytsura Collection of Things; fales") }
+      it { should eql("The Title, August 19, 1992; Bytsura Collection of Things; Box:2, Folder: 3; fales") }
     end
    
   end
