f788395b02666cc37c91dfac27fe7b837bf5b838
B Alter <barnaby.alter@gmail.com>
2015-04-15 11:39:33 -0400

    Set force_ssl to true in config and change relative root to actual root since we're making this point to dev.specialcollections.library.nyu.edu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_controller.relative_url_root = '/findingaids'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -40,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
