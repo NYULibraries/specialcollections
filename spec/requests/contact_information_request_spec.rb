40a77d0483cdda9e69f74398814d38750c47651c
B Alter <barnaby.alter@gmail.com>
2015-04-17 12:22:42 -0400

    Moved help tests into HelpController spec since that is what is being tested

--- spec/requests/contact_information_request_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe "contact information" do
-  it "returns the Contact information page" do
-    get "/help/contact_information"
-    expect(response.status).to be == 200
-    expect(response.body).to include("New York University Polytechnic School of Engineering")
-  end
-end
