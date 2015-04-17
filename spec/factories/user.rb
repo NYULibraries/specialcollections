41630cb4b135857abd5e693b3f820d9ed0ab3250
Barnaby <barnaby.alter@nyu.edu>
2015-04-16 23:10:21 -0400

    Added firstname to user spec

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,10 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:username) {|n| "test123#{n}"} 
+    sequence(:username) {|n| "test123#{n}"}
     sequence(:email) { |n| "test123#{n}@library.edu" }
+    firstname "Ptolemy"
   end
 
-  factory :user_dev, class:User do
+  factory :user_dev, class: User do
+    firstname "Ramses"
   	username "dev123"
   	email    "dev.loper@library.edu"
   end
