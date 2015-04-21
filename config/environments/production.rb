89fd29ead0c5eeb48cce838048d43f86d2e2156c
B Alter <barnaby.alter@gmail.com>
2015-04-21 16:12:36 -0400

    Cleaned up routes to exclude unnecessary ones; changes root url to /search

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_controller.relative_url_root = '/search'
+
   config.action_mailer.default_options = {from: 'no-reply@library.nyu.edu'}
 
   # Code is not reloaded between requests.
