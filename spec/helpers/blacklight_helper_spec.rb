b381625391d766c7af0a97a61a49b2a03e953393
ekate <ekate@nyu.edu>
2015-07-06 17:22:05 -0400

    Added spec for blacklight_helper

---
+++ spec/helpers/blacklight_helper_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+describe BlacklightHelper do
+
+  include ApplicationHelper
+  include Blacklight::BlacklightHelperBehavior
+
+  let(:field) { "unittitle_ssm" }
+  let(:solr_document) { create(:solr_document) }
+  let(:blacklight_config) { create(:blacklight_config) }
+  let(:document) {{ document: solr_document, field: field }}
+ 
+  describe "#link_to_document" do
+    let(:opts) { {:counter => nil} }
+    subject { link_to_document(document[:document],document[:document][:field],opts)}
+    context "when document has title" do
+      it { should eql("<a href=\"http://dlib.nyu.edu/findingaids/html/fales/testead/dsc.html#123\" target=\"_blank\">Guide to titling finding aids</a>") }
+    end
+    context "when document doesn't have title" do
+      let(:solr_document) { create(:solr_document, unittitle: []) }
+      it { should eql( "<a href=\"http://dlib.nyu.edu/findingaids/html/fales/testead/dsc.html#123\" target=\"_blank\">No Title</a>") }
+    end
+  end
+end
