9db36bedb216a1dd2ca558fcfa1704c4ac4c4a64
Eric Griffis <eric.griffis@nyu.edu>
2016-10-25 13:09:46 -0400

    Remove redundant include

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ActiveRecord::Base
   if Blacklight::Utils.needs_attr_accessible?
     attr_accessible :email, :password, :password_confirmation
   end
-  # Connects this user object to Blacklights Bookmarks.
-  include Blacklight::User
   # Connects this user object to Blacklights Bookmarks and Folders.
   include Blacklight::User
   devise :omniauthable, omniauth_providers: [:nyulibraries]
