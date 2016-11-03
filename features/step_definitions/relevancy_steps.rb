4e55d272e8543191299e581d1e9c35d5b8506926
Barnaby Alter <barnaby.alter@nyu.edu>
2016-11-03 13:57:50 -0400

    Updated some cukes

--- features/step_definitions/relevancy_steps.rb
+++ features/step_definitions/relevancy_steps.rb
@@ -1,7 +1,8 @@
-Then(/^the document with title "(.*?)" should appear in the "(.*?)" first results$/) do |title, number|
-  solr_docs_titles(documents_list_container).should include(title).in_first(number).results
+Then(/^the document with title "(.*?)" should appear in the first "(.*?)" results$/) do |title, number|
+  n = number.to_i - 1 rescue document_titles.count
+  expect(document_titles[0..n]).to include title
 end
 
-Then(/^the document with title "(.*?)" should appear as first result$/) do |title|
-  solr_docs_titles(documents_list_container).should include(title).as_first_result
+Then(/^the document with title "(.*?)" should appear as the first result$/) do |title|
+  expect(document_titles.first).to eql title
 end
