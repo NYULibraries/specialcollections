4bfb9888f0f9dfc8984c8a7aa72332857befe00e
BAlter <barnaby.alter@nyu.edu>
2018-03-01 10:41:46 -0500

    Updated to ruby 2.3.6

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ require 'formaggio/capistrano'
 
 set :app_title, "findingaids"
 
-set :rvm_ruby_string, "2.3.3"
+set :rvm_ruby_string, "2.3.6"
 
 namespace :deploy do
   # Remove EADs from server after deploying
