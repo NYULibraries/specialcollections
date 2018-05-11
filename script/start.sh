9ac93d0c478cd642af9f6e3c6f1f6445c22c067e
BAlter <barnaby.alter@nyu.edu>
2018-05-11 14:29:16 -0400

    Fix unicorn startup script to use correct rack_env var

--- script/start.sh
+++ script/start.sh
@@ -4,7 +4,7 @@
 # ./start.sh {ENVIRONMENT}
 
 # Stop existing process first, if any
-./script/stop.sh $1
+./script/stop.sh
 
 echo "Starting up unicorn..."
-bundle exec unicorn -c config/unicorn/config.rb -E $2
+bundle exec unicorn -c config/unicorn/config.rb -E $1
