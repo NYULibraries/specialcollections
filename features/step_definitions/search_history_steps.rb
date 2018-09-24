eee92cbdd07ca46686533ffe08174d6159ef8eba
Eric Griffis <eric.griffis@nyu.edu>
2018-09-24 14:23:54 -0400

    Update test image to run selenium; default cucumber tests to selenium

--- features/step_definitions/search_history_steps.rb
+++ features/step_definitions/search_history_steps.rb
@@ -16,6 +16,12 @@ Then(/^I should see no saved searches links$/) do
   expect(page).to_not have_css(".table")
 end
 
+When(/^I click on the "(.+)" button, accepting the alert "(.+)"$/) do |button_text, alert_text|
+  accept_alert alert_text do
+    click_on(button_text)
+  end
+end
+
 Then(/^I should see an alert "(.+)"$/) do |alert_text|
   expect(page).to have_text alert_text
   expect(page).to have_css ".alert", text: alert_text
