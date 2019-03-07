eb76f86666a933091da6767eeea42657ddd37283
BAlter <barnaby.alter@nyu.edu>
2019-03-07 17:14:01 -0500

    Add metrics defaults

--- config.ru
+++ config.ru
@@ -1,6 +1,15 @@
 # This file is used by Rack-based servers to start the application.
+require 'rack'
+require_relative 'config/metrics'
 
 require ::File.expand_path('../config/environment',  __FILE__)
+
+use Rack::Deflater
+# Run prometheus middleware to collect default metrics
+use Prometheus::Middleware::CollectorWithExclusions
+# Run prometheus exporter to have a /metrics endpoint that can be scraped
+# The endpoint will only be available to prometheus
+use Prometheus::Middleware::Exporter
 
 if ENV['DOCKER']
   map(ENV['RAILS_RELATIVE_URL_ROOT'] || "/") { run Findingaids::Application }
---
+++ config/metrics.rb
@@ -0,0 +1,43 @@
+require 'prometheus/middleware/collector'
+require 'prometheus/middleware/exporter'
+
+module Prometheus::Middleware
+  class CollectorWithExclusions < Collector
+    def initialize(app, options = {})
+      @exclude = EXCLUDE
+      options[:counter_label_builder] = SHARED_CUSTOM_LABEL_BUILDER
+      options[:duration_label_builder] = SHARED_CUSTOM_LABEL_BUILDER
+      options[:metrics_prefix] = ENV['PROMETHEUS_METRICS_PREFIX']
+
+      super(app, options)   
+    end
+
+    def call(env)
+      if @exclude && @exclude.call(env)
+        @app.call(env)
+      else
+        super(env)
+      end
+    end
+
+  protected
+
+    EXCLUDE = proc do |env|
+      (env['PATH_INFO'] == '/healthcheck') || (env['PATH_INFO'] == '/metrics')
+    end
+
+    SHARED_CUSTOM_LABEL_BUILDER = proc do |env, code|
+      {
+        code:         code,
+        method:       env['REQUEST_METHOD'].downcase,
+        host:         env['HTTP_HOST'].to_s,
+        path:         env['PATH_INFO'],
+        querystring:  env['QUERY_STRING'],
+        route:        env['sinatra.route'],
+        app:          "specialcollections"
+      }
+    end
+
+  end
+end
+
