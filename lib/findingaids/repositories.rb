25a25aadc568ac9788a04f387c28fcc8e46f2a2c
jgpawletko <jgpawletko@gmail.com>
2023-06-14 04:06:18 +0000

    update Findingaids::Repositories.repositories
    Update Findingaids::Repositories.repositories method to accommodate
      differences in the repositories values between the legacy and
      FINDINGAIDS_2022_MIGRATION configurations.

--- lib/findingaids/repositories.rb
+++ lib/findingaids/repositories.rb
@@ -3,7 +3,23 @@ module Findingaids
     include Enumerable
 
     def self.repositories
-      @repositories ||= YAML.load_file( File.join(Rails.root, "config", "repositories.yml") )["Catalog"]["repositories"]
+      return @repositories if @repositories
+
+      # This code allows the values from the YAML file to be overridden based on
+      # whether or not the FINDINGAIDS_2022_MIGRATION environment variable is set
+
+      # load repositories.yml file
+      yaml_contents = YAML.load_file( File.join(Rails.root, "config", "repositories.yml") )
+
+      # extract legacy repositories
+      @repositories = yaml_contents["Catalog"]["repositories"]
+
+      # override repositories if needed
+      if ENV['FINDINGAIDS_2022_MIGRATION']
+        @repositories.merge!(yaml_contents["CatalogOverride"]["repositories"])
+      end
+
+      @repositories
     end
 
     extend Forwardable
