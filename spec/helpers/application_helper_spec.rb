ae92a22a26d79453a5a159db294798a327be493d
jgpawletko <jgpawletko@gmail.com>
2023-06-14 19:11:17 +0000

    update ApplicationHelper examples
    Add repository['url_safe_display'] assertions to ApplicationHelper
      examples

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -61,6 +61,7 @@ describe ApplicationHelper do
       context "when repository is Fales" do
         subject(:repository) { repositories['fales'] }
         it { expect(repository['display']).to eql("The Fales Library & Special Collections") }
+        it { expect(repository['url_safe_display']).to eql("The Fales Library %26 Special Collections") }
         it { expect(repository['url']).to eql("fales") }
         it { expect(repository['admin_code']).to eql("fales") }
       end
@@ -68,6 +69,7 @@ describe ApplicationHelper do
       context "when repository is Tamiment " do
         subject(:repository) { repositories['tamiment'] }
         it { expect(repository['display']).to eql("Tamiment Library & Wagner Labor Archives") }
+        it { expect(repository['url_safe_display']).to eql("Tamiment Library %26 Wagner Labor Archives") }
         it { expect(repository['url']).to eql("tamiment") }
         it { expect(repository['admin_code']).to eql("tamwag") }
       end
@@ -75,6 +77,7 @@ describe ApplicationHelper do
       context "when repository is NYU Archives " do
         subject(:repository) { repositories['universityarchives'] }
         it { expect(repository['display']).to eql("New York University Archives") }
+        it { expect(repository['url_safe_display']).to eql("New York University Archives") }
         it { expect(repository['url']).to eql("universityarchives") }
         it { expect(repository['admin_code']).to eql("archives") }
       end
@@ -82,6 +85,7 @@ describe ApplicationHelper do
       context "when repository is NYHS" do
         subject(:repository) { repositories['nyhistory'] }
         it { expect(repository['display']).to eql("New-York Historical Society") }
+        it { expect(repository['url_safe_display']).to eql("New-York Historical Society") }
         it { expect(repository['url']).to eql("nyhistory") }
         it { expect(repository['admin_code']).to eql("nyhs") }
       end
@@ -89,6 +93,7 @@ describe ApplicationHelper do
       context "when repository is BHS" do
         subject(:repository) { repositories['brooklynhistory'] }
         it { expect(repository['display']).to eql("Center for Brooklyn History") }
+        it { expect(repository['url_safe_display']).to eql("Center for Brooklyn History") }
         it { expect(repository['url']).to eql("brooklynhistory") }
         it { expect(repository['admin_code']).to eql("bhs") }
       end
@@ -96,6 +101,7 @@ describe ApplicationHelper do
       context "when repository is Poly Archives" do
         subject(:repository) { repositories['poly'] }
         it { expect(repository['display']).to eql("Poly Archives") }
+        it { expect(repository['url_safe_display']).to eql("Poly Archives") }
         it { expect(repository['url']).to eql("poly") }
         it { expect(repository['admin_code']).to eql("poly") }
       end
@@ -103,6 +109,7 @@ describe ApplicationHelper do
       context "when repository is RISM" do
         subject(:repository) { repositories['rism'] }
         it { expect(repository['display']).to eql("Research Institute for the Study of Man") }
+        it { expect(repository['url_safe_display']).to eql("Research Institute for the Study of Man") }
         it { expect(repository['url']).to eql("rism") }
         it { expect(repository['admin_code']).to eql("rism") }
       end
@@ -110,6 +117,7 @@ describe ApplicationHelper do
       context "when repository is NYUAD" do
         subject(:repository) { repositories['nyuad'] }
         it { expect(repository['display']).to eql("NYU Abu Dhabi Archives and Special Collections") }
+        it { expect(repository['url_safe_display']).to eql("NYU Abu Dhabi Archives and Special Collections") }
         it { expect(repository['url']).to eql("nyuad") }
         it { expect(repository['admin_code']).to eql("nyuad") }
       end
@@ -117,6 +125,7 @@ describe ApplicationHelper do
       context "when repository is VLP" do
         subject(:repository) { repositories['vlp'] }
         it { expect(repository['display']).to eql("Villa La Pietra") }
+        it { expect(repository['url_safe_display']).to eql("Villa La Pietra") }
         it { expect(repository['url']).to eql("vlp") }
         it { expect(repository['admin_code']).to eql("vlp") }
       end
@@ -155,7 +164,7 @@ describe ApplicationHelper do
         subject(:repository) { repositories['tamiment'] }
         it { expect(repository['display']).to eql("Tamiment Library & Wagner Labor Archives") }
         it { expect(repository['url']).to eql("tamiment") }
-        it { expect(repository["url_safe_display"]).to eql("Tamiment Library %26 Wagner Labor Archives")
+        it { expect(repository["url_safe_display"]).to eql("Tamiment Library %26 Wagner Labor Archives") }
         it { expect(repository['admin_code']).to eql("tamwag") }
       end
     end
