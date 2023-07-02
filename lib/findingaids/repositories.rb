4923c5d638340e77e4f4cbfb7a7a66734b9097c1
jgpawletko <jgpawletko@gmail.com>
2023-07-02 16:27:11 +0000

    update self.repositories
    Move repositories initialization inside a begin/end block

--- lib/findingaids/repositories.rb
+++ lib/findingaids/repositories.rb
@@ -3,9 +3,10 @@ module Findingaids
     include Enumerable
 
     def self.repositories
-      repositories_yaml_file = ENV['FINDINGAIDS_2022_MIGRATION'] ? "repositories-findingaids_2022_migration.yml" : "repositories.yml"
-
-      @repositories ||= YAML.load_file( File.join(Rails.root, "config", repositories_yaml_file) )["Catalog"]["repositories"]
+      @repositories ||= begin
+                          repositories_yaml_file = ENV['FINDINGAIDS_2022_MIGRATION'] ? "repositories-findingaids_2022_migration.yml" : "repositories.yml"
+                          YAML.load_file( File.join(Rails.root, "config", repositories_yaml_file) )["Catalog"]["repositories"]
+                        end
     end
 
     extend Forwardable
