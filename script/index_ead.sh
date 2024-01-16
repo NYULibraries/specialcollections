a908d94d515ab06396a5579036529089cc30049a
Eric Griffis <eric.griffis@nyu.edu>
2024-01-16 17:36:03 -0500

    Fabify - add linux amd64 binary; add scripts for indexing commits, eads

---
+++ script/index_ead.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+# assumed location from ead_indexer
+EAD_DIR="$PWD/findingaids_eads"
+# check that specified ead file exists
+ead_path="$EAD_DIR/$1"
+if ![ -f "$ead_path" ]; then
+  echo "Cannot index EAD=$1 since it does not exist at $ead_path"
+  exit 1
+fi
+# fabify 
+echo "Fabifying EAD=$1"
+tmp_ead_path="/tmp/$1"
+./bin/nyudlts/dlts-finding-aids-fasb/fasb fabify ead $ead_path > $tmp_ead_path
+mv $tmp_ead_path $ead_path
+# index
+bundle exec rake ead_indexer:index EAD=$1
---
+++ script/index_last_commit.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+# assumed location from ead_indexer
+EAD_DIR="$PWD/findingaids_eads"
+# fabify any files changed in last commit
+EAD_LIST=$(git -C $EAD_DIR diff --name-only HEAD HEAD~1)
+for ead in $EAD_LIST
+do
+  ead_path="$EAD_DIR/$ead"
+  if [ -f "$ead_path" ]; then
+    echo "Fabifying EAD=$ead"
+    tmp_ead_path="/tmp/$ead"
+    ./bin/nyudlts/dlts-finding-aids-fasb/fasb fabify ead $ead_path > $tmp_ead_path
+    mv $tmp_ead_path $ead_path
+  else
+    echo "Skipping EAD $ead since it does not exist at $ead_path: deletion?"
+  fi
+done
+# ead_indexer gathers files changed from commit; knows how to read deletions
+bundle exec rake ead_indexer:reindex_changed
