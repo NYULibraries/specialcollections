db6d1b4bce576db518b26591c5a2fa415be01370
Eric Griffis <eric.griffis@nyu.edu>
2024-02-02 15:26:40 -0500

    Fabify - modify tmp ead path >> fix base path

--- script/index_ead.sh
+++ script/index_ead.sh
@@ -10,7 +10,7 @@ if ![ -f "$ead_path" ]; then
 fi
 # fabify 
 echo "Fabifying EAD=$1"
-tmp_ead_path="$1-tmp"
+tmp_ead_path="$ead_path-tmp"
 ./bin/nyudlts/dlts-finding-aids-fasb/fasb fabify ead $ead_path > $tmp_ead_path
 mv $tmp_ead_path $ead_path
 # index
